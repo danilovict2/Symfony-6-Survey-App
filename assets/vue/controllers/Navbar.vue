2afa927df368affb96df131d0498a5dc37eba821
danilovict2 <danilosindric@gmail.com>
2023-08-31 18:43:47 +0200

    Added user info to navbar

--- assets/vue/controllers/Navbar.vue
+++ assets/vue/controllers/Navbar.vue
@@ -106,18 +106,12 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue';
 
 const props = defineProps({
-    route: String
+    route: String,
+    user: Object
 });
-
-const user = {
-    name: 'Tom Cook',
-    email: 'tom@example.com',
-    imageUrl:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-};
 
 const navigation = [
     { name: 'Dashboard', href: '/' },
-    { name: 'Survey', href: '#' },
+    { name: 'Survey', href: '/surveys' },
 ];
 </script>
