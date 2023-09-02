0100fabfa7c00458216a7257fa2e7ad7d7fa829f
danilovict2 <danilosindric@gmail.com>
2023-09-02 17:12:37 +0200

    Created SurveyQuestionAnswer Entity

---
+++ src/Repository/SurveyQuestionAnswerRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\SurveyQuestionAnswer;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<SurveyQuestionAnswer>
+ *
+ * @method SurveyQuestionAnswer|null find($id, $lockMode = null, $lockVersion = null)
+ * @method SurveyQuestionAnswer|null findOneBy(array $criteria, array $orderBy = null)
+ * @method SurveyQuestionAnswer[]    findAll()
+ * @method SurveyQuestionAnswer[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SurveyQuestionAnswerRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, SurveyQuestionAnswer::class);
+    }
+}
