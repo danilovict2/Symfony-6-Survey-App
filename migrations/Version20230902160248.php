1bb82df9a5984b319b7f3b4cc4f5192354b580aa
danilovict2 <danilosindric@gmail.com>
2023-09-02 19:09:44 +0200

    minor changes

--- migrations/Version20230902160248.php
+++ migrations/Version20230902160248.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20230902160248 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,7 +16,6 @@ final class Version20230902160248 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE survey ADD created_at TIMESTAMP(0) WITHOUT TIME ZONE');
         $this->addSql('UPDATE survey SET created_at = CURRENT_TIMESTAMP');
         $this->addSql('ALTER TABLE survey ALTER COLUMN created_at SET NOT NULL');
@@ -28,7 +24,6 @@ final class Version20230902160248 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE survey DROP created_at');
     }
